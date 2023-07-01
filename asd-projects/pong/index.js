ad3c103fe8c95fe28795830227731e480034572f
misla25 <misla25@bfhsla.org>
2023-07-01 18:10:15 +0000

    new font

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -176,7 +176,6 @@ function check(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 function doCollide(obj1, obj2){
-  
   if (obj1.x < obj2.x + obj2.width && obj1.x + obj1.width > obj2.x && obj1.y < obj2.y + obj2.height && obj1.y + obj1.height > obj2.y) {
     return true;
   } else {
@@ -205,6 +204,5 @@ function moveObject(object) {
     // turn off event handlers
     $(document).off();
   }
-
 }
 endGame();
