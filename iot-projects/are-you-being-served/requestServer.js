ae7aa0a42ce0c87605d3d22f15958eaaea1bea1c
IoT <iot@Supers-Mac-mini.local>
2023-09-19 19:44:18 -0500

    stared requjest

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,17 @@
 // requestServer.js file
+const http = require("http");
+const request = require("request");
+const port = 7777;
+// example http server creation
+http.createServer(function(req,res){
+    //handle response
+    //request("https://github.com/misla25",request());
+}).listen(port);
+// function(req,res){
+//     request("https://github.com/misla25");
+// }
+request({url:"https://github.com/misla25"}, function(err, resp, body) {
+    if (!err && resp.statusCode === 200) {
+      console.log(body.data);
+    }
+});
