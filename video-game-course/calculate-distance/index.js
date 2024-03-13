55b0354868a194540017dfd7afd4aa0585df6b1f
misla25 <misla25@bfhsla.org>
2024-03-13 00:57:24 +0000

    i could not import the index.js file...

--- video-game-course/calculate-distance/index.js
+++ video-game-course/calculate-distance/index.js
@@ -1,8 +1,9 @@
 // TODO 4: Add a parameter for your gaming library in the index.js module:
-(function(window, createjs, opspark) {
+(function(window, createjs, opspark, no) {
   // OUR MODULE CODE GOES BELOW HERE //
 
   console.log('index.js initialized!');
+  console.log(no);
   
   /*
    * In this project, we'll use our getDistance() method to 
@@ -26,7 +27,8 @@
     radius = 25, // the radius of our two circles
     shapeUp = new createjs.Shape(), // the up state: the mouse is NOT intersecting
     shapeOver = new createjs.Shape(); // the over state: the mouse IS intersecting
-
+    shapeUpRect =  new createjs.Shape();
+    shapeOverRect = new createjs.Shape();
   /*
    * Draw two circles, up for when the mouse is not touching it, over
    * for when the mouse is touching it. Note, the createjs API for drawing
@@ -34,7 +36,11 @@
    */
   shapeUp.graphics.beginFill('blue').drawCircle(0, 0, radius);
   shapeOver.graphics.beginFill('red').drawCircle(0, 0, radius);
+  shapeUpRect.graphics.beginFill('purple').drawRect(100, 100, 50,50);
+  shapeOverRect.graphics.beginFill('pink').drawRect(100, 100, 50,50);
+  
   shapeOver.alpha = 0;
+ 
   shapeUp.x = shapeOver.x = canvas.width / 2;
   shapeUp.y = shapeOver.y = canvas.height / 2;
 
@@ -49,7 +55,7 @@
   textfield.x = (canvas.width - textBounds.width) / 2;
   textfield.y = canvas.height / 2 + 50;
   
-  stage.addChild(shapeUp, shapeOver, textfield);
+  stage.addChild(shapeUp, shapeOver, textfield, shapeOverRect, shapeUpRect);
   
   // The update() method is called 60 times a second //
   function update(event) {
@@ -57,15 +63,33 @@
      * TODO 5: use getDistance to calculate the distance between shapeUp and 
      * the mouse. Store the result in a variable called distance:
      */
-    
-    
+    let mouse = {
+      x: stage.mouseX,
+      y: stage.mouseY
+    }
+    var distance = no.phyz.calculateDistance(shapeUp, mouse);
+    var distance2 = no.phyz.calculateDistance(shapeUpRect, mouse);
     
     /*
      * TODO 6: Check if the mouse is within the area of shapeUp, and set the 
      * alpha property of shapeUp accordingly:
      */
-    
-    
+    if( distance < radius){
+      shapeUp.alpha = 0;
+      shapeOver.alpha = 1;
+    }
+    else{
+      shapeUp.alpha = 1;
+      shapeOver.alpha = 0;
+    }
+    if(distance < 50){
+      shapeUpRect.alpha = 0;
+      shapeOverRect.alpha = 1;
+    }
+    else{
+      shapeUpRect.alpha = 1;
+      shapeOverRect.alpha = 0;
+    }
     /*
      * Update the textfield with the current distance between the mouse and 
      *the edge of the shapeUp
@@ -100,4 +124,4 @@
   // OUR MODULE CODE GOES ABOVE HERE //
   
 // TODO 3: Pass your gaming library into the index.js module:
-}(window, window.createjs, window.opspark));
+}(window, window.createjs, window.opspark, window.no));
